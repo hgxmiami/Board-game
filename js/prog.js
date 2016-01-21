65094688d4689cdff28df5d6d75f79e3500e0a93
Hugo Giraldo <hgxmiami00@gmail.com>
2016-01-20 21:38:29 -0500

    continue game
    con

---
+++ js/prog.js
@@ -0,0 +1,10 @@
+function dieRoll(){
+    return Math.round(Math.random() * 11 + 1);
+
+}
+
+var bee = document.getElementById("btn");
+bee.onclick = function(){
+    console.log(dieRoll());
+    document.getElementById("btn").innerHTML=dieRoll();
+};
